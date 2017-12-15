e3de67929f1861f287c775b94581a1d45621a8dc
libingxin <396249450@qq.com>
2017-12-15 17:55:20 +0800

    表格的使用，不够灵活

--- src/view/generalize.vue
+++ src/view/generalize.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>{{username}}，欢迎回来</h3>
-    generalize
+    <Table border :columns="columns7"  @on-selection-change="show" :data="data6"></Table>
   </div>
 </template>
 
@@ -10,7 +10,119 @@ export default {
   name: 'generalize',
   data () {
     return {
-      username: '沈阳礼品回收'
+      abcd:true,
+      username: '沈阳礼品回收',
+      columns7: [
+                    {
+                        title: 'Name',
+                        key: 'name',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Icon', {
+                                    props: {
+                                        type: 'person'
+                                    }
+                                }),
+                                h('strong', params.row.name)
+                            ]);
+                        }
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    },
+                    {
+                        title: 'Action',
+                        key: 'action',
+                        width: 150,
+                        align: 'center',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Input', {
+                                    props: {
+                                      "value":params.row.select
+                                    },
+                                    on: {
+                                        change: () => {
+                                            this.show(params.index)
+                                        }
+                                    }
+                                })
+                            ]);
+                        }
+                    },
+                    {
+                        title: 'Action',
+                        key: 'action',
+                        width: 150,
+                        align: 'center',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'primary',
+                                        size: params.row.select,
+                                        icon:'checkmark-round'
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.show(params.index)
+                                        }
+                                    }
+                                }, params.index),
+                                h('Button', {
+                                    props: {
+                                        type: 'error',
+                                        size: 'small'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.remove(params.index)
+                                        }
+                                    }
+                                }, 'Delete')
+                            ]);
+                        }
+                    }
+                ],
+                data6: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        select:false
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        select:'small'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        select:true
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        select:true
+                    }
+                ]
+    }
+  },
+  methods: {
+    show (a) {
+      console.log(a);
     }
   }
 }
--- src/view/help.vue
+++ src/view/help.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <h3>{{username}}，欢迎回来</h3>
-    help
-  </div>
+    <div>
+    <i-switch v-model="showCheckbox" style="margin-right: 5px"></i-switch>
+    <Table :border="showBorder" @on-selection-change="abc" :stripe="showStripe" :show-header="showHeader" :height="fixedHeader ? 250 : ''" :size="tableSize" :data="tableData3" :columns="tableColumns3"></Table>
+    </div>
 </template>
 
 <script>
@@ -10,8 +10,144 @@ export default {
   name: 'help',
   data () {
     return {
-      username: '沈阳礼品回收'
+      username: '沈阳礼品回收',
+      tableData3: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ],
+                showBorder: false,
+                showStripe: false,
+                showHeader: true,
+                showIndex: true,
+                showCheckbox: false,
+                fixedHeader: false,
+                tableSize: 'default'
     }
-  }
+  },
+    computed: {
+        tableColumns3 () {
+            let columns = [];
+            if (this.showCheckbox) {
+                columns.push({
+                    type: 'selection',
+                    width: 60,
+                    align: 'center'
+                })
+            }
+            if (this.showIndex) {
+                columns.push({
+                    type: 'index',
+                    width: 60,
+                    align: 'center'
+                })
+            }
+            columns.push({
+                title: 'Date',
+                key: 'date',
+                sortable: true
+            });
+            columns.push({
+                title: 'Name',
+                key: 'name',
+                sortable: true
+            });
+            columns.push({
+                title: 'Age',
+                key: 'age',
+                sortable: true,
+                filters: [
+                    {
+                        label: 'Greater than 25',
+                        value: 1
+                    },
+                    {
+                        label: 'Less than 25',
+                        value: 2
+                    }
+                ],
+                filterMultiple: false,
+                filterMethod (value, row) {
+                    if (value === 1) {
+                        return row.age > 25;
+                    } else if (value === 2) {
+                        return row.age < 25;
+                    }
+                }
+            });
+            columns.push({
+                title: 'Address',
+                key: 'address',
+                filters: [
+                    {
+                        label: 'New York',
+                        value: 'New York'
+                    },
+                    {
+                        label: 'London',
+                        value: 'London'
+                    },
+                    {
+                        label: 'Sydney',
+                        value: 'Sydney'
+                    }
+                ],
+                filterMethod (value, row) {
+                    return row.address.indexOf(value) > -1;
+                }
+            });
+            return columns;
+        }
+    },
+    methods: {
+      abc (a) {
+        console.log(a);
+      }
+    }
 }
 </script>
